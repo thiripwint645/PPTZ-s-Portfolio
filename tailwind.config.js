4fa9dc66335ef865947ad2af132eba1415f285e3
thiripwint645 <thiripwint21@gmail.com>
2023-04-20 22:14:48 +0630

    second-commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,css}"],
+
   theme: {
     extend: {},
   },
-
-  plugins: [require("daisyui")],
-
-  daisyui: {
-    themes: [
-      "light",
-      "dark",
-      "cupcake",
-      "bumblebee",
-      "emerald",
-      "corporate",
-      "synthwave",
-      "retro",
-      "cyberpunk",
-      "valentine",
-      "halloween",
-      "garden",
-      "forest",
-      "aqua",
-      "lofi",
-      "pastel",
-      "fantasy",
-      "wireframe",
-      "black",
-      "luxury",
-      "dracula",
-      "cmyk",
-      "autumn",
-      "business",
-      "acid",
-      "lemonade",
-      "night",
-      "coffee",
-      "winter",
-    ],
-  },
+  plugins: [],
 };
